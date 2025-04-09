5df92b2e57ce1ef5c17f6ddee9d2b08f4c501a63
Shubhani Pandey <shubhani.pandey@nagarro.com>
2025-04-09 20:44:18 +0530

    increasing rate limit' git push ' '

--- user-service/utils/rate_limit.py
+++ user-service/utils/rate_limit.py
@@ -4,7 +4,7 @@ from flask_limiter.util import get_remote_address
 def setup_limiter(app):
     limiter = Limiter(
         key_func=get_remote_address,
-        default_limits=["200 per day", "50 per hour"]
+        default_limits=["20000 per day", "5000 per hour"]
     )
     limiter.init_app(app)
     return limiter
